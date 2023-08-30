a354e4c97af58775fe0d932e1c66b76934d9e2fd
Azikenp <azikenp26@gmail.com>
2023-08-30 22:18:58 +0100

    additions

---
+++ src/services/hnApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const baseURL = "https://hacker-news.firebaseio.com/v0/";
+export const newStoriesUrl = `${baseURL}newstories.json`;
+export const storyUrl = `${baseURL}item/`;
+
+export const getStoryIds = async () => {
+  const result = await axios.get(newStoriesUrl).then((data) => data);
+  return result;
+};
